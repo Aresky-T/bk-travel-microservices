fa920eef5bb5c81ae3a0110210dd1979df7baed3
Aresky-T <tn6354103@gmail.com>
2024-03-14 16:34:02 +0700

    init base services

---
+++ tour-service/src/main/java/com/aresky/tourservice/TourServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aresky.tourservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourServiceApplication.class, args);
+	}
+
+}
---
+++ tour-service/src/test/java/com/aresky/tourservice/TourServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aresky.tourservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
