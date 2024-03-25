6d01f177ecafbf2696d07030f94b04f5633a7afb
Aresky-T <tn6354103@gmail.com>
2024-03-25 22:12:15 +0700

    update service config

--- account-service/src/main/java/com/aresky/accountservice/config/ApplicationConfig.java
+++ account-service/src/main/java/com/aresky/accountservice/config/ApplicationConfig.java
@@ -3,14 +3,31 @@ package com.aresky.accountservice.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.ReactivePageableHandlerMethodArgumentResolver;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer;
 
 @Configuration
-public class ApplicationConfig {
+@EnableSpringDataWebSupport
+public class ApplicationConfig implements WebFluxConfigurer {
+
+    @Override
+    public void configureArgumentResolvers(ArgumentResolverConfigurer configurer) {
+        configurer.addCustomResolver(new ReactivePageableHandlerMethodArgumentResolver());
+    }
 
     @LoadBalanced
     @Bean
     RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    PasswordEncoder encoder (){
+        return new BCryptPasswordEncoder();
     }
 }
