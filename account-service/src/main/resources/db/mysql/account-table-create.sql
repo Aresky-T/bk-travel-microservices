1f5b630c4d4e080ec1deb9ecf8815a3a26493747
Aresky-T <tn6354103@gmail.com>
2024-03-25 22:10:46 +0700

    update chagelog and sql documents

--- account-service/src/main/resources/db/mysql/account-table-create.sql
+++ account-service/src/main/resources/db/mysql/account-table-create.sql
@@ -1,14 +1,10 @@
 CREATE TABLE IF NOT EXISTS `account` (
-    `id` BIGINT NOT NULL AUTO_INCREMENT,
-    `avatar_url` varchar(255) DEFAULT NULL,
-    `full_name` varchar(100) DEFAULT NULL,
-    `address` varchar(255) DEFAULT NULL,
-    `phone` varchar(20) DEFAULT NULL,
-    `date_of_birth` date DEFAULT NULL,
-    `gender` enum('MALE', 'FEMALE', 'OTHER') DEFAULT NULL,
-    `auth_id` int NOT NULL,
-    `created_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE KEY`auth_id`(`auth_id`),
-    PRIMARY KEY (`id`)
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    `username` VARCHAR(30) NOT NULL UNIQUE,
+    `email` VARCHAR(100) NOT NULL UNIQUE,
+    `password` VARCHAR(255) NOT NULL,
+    `role` ENUM('USER', 'ADMIN', 'EMPLOYEE') NOT NULL DEFAULT 'USER',
+    `activation_status` ENUM('ACTIVE', 'BLOCKED') NOT NULL DEFAULT 'ACTIVE',
+    `online_status` ENUM('ONLINE', 'OFFLINE') NOT NULL DEFAULT 'OFFLINE',
+    created_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
