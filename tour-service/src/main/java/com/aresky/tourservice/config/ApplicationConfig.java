d070b3d60c5d737067371c21fa589f355a3e1c28
Aresky-T <tn6354103@gmail.com>
2024-03-31 20:39:44 +0700

    add config classes

---
+++ tour-service/src/main/java/com/aresky/tourservice/config/ApplicationConfig.java
@@ -0,0 +1,46 @@
+package com.aresky.tourservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.ReactivePageableHandlerMethodArgumentResolver;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer;
+
+import com.aresky.tourservice.config.DatabaseConfig.Server;
+
+import io.netty.util.internal.StringUtil;
+import io.r2dbc.spi.ConnectionFactories;
+import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactoryOptions;
+import io.r2dbc.spi.ConnectionFactoryOptions.Builder;
+
+@EnableWebFlux
+@EnableSpringDataWebSupport
+@Configuration
+public class ApplicationConfig implements WebFluxConfigurer {
+
+    @Override
+    public void configureArgumentResolvers(ArgumentResolverConfigurer configurer) {
+        configurer.addCustomResolver(new ReactivePageableHandlerMethodArgumentResolver());
+    }
+
+    @Bean
+    ConnectionFactory connectionFactory(DatabaseConfig databaseConfig) {
+        Server server = databaseConfig.getServer();
+
+        ConnectionFactoryOptions baseOptions = ConnectionFactoryOptions.parse(server.getUrl());
+        Builder builder = ConnectionFactoryOptions.builder().from(baseOptions);
+
+        if (!StringUtil.isNullOrEmpty(server.getUsername())) {
+            builder = builder.option(ConnectionFactoryOptions.USER, server.getUsername());
+        }
+
+        if (!StringUtil.isNullOrEmpty(server.getPassword())) {
+            builder = builder.option(ConnectionFactoryOptions.PASSWORD, server.getUsername());
+        }
+
+        return ConnectionFactories.get(builder.build());
+    }
+}
---
+++ tour-service/src/main/java/com/aresky/tourservice/config/DatabaseConfig.java
@@ -0,0 +1,97 @@
+package com.aresky.tourservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Configuration properties for Database server of BK Travel
+ *
+ * @author Aresky
+ * @since 1.0.0
+ */
+@Component
+@ConfigurationProperties("bk-travel.config.database")
+public class DatabaseConfig {
+    private final Server server = new Server();
+
+    public Server getServer() {
+        return this.server;
+    }
+
+    public static class Server {
+        private String driver = "mysql";
+        private String protocol = "mysql";
+        private String host = "localhost";
+        private Integer port = 3306;
+        private String url;
+        private String database;
+        private String username;
+        private String password;
+
+        public String getDriver() {
+            return driver;
+        }
+
+        public void setDriver(String driver) {
+            this.driver = driver;
+        }
+
+        public String getProtocol() {
+            return protocol;
+        }
+
+        public void setProtocol(String protocol) {
+            this.protocol = protocol;
+        }
+
+        public String getHost() {
+            return host;
+        }
+
+        public void setHost(String host) {
+            this.host = host;
+        }
+
+        public Integer getPort() {
+            return port;
+        }
+
+        public void setPort(Integer port) {
+            this.port = port;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getDatabase() {
+            return database;
+        }
+
+        public void setDatabase(String database) {
+            this.database = database;
+        }
+
+    }
+
+}
