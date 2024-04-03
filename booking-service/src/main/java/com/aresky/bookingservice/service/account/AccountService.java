fa67dda3f5f2f68e4ca6e053b7287bf4431ed8b6
Aresky-T <tn6354103@gmail.com>
2024-04-03 14:50:01 +0700

    add WebClient for TourService, AccountService

--- booking-service/src/main/java/com/aresky/bookingservice/service/account/AccountService.java
+++ booking-service/src/main/java/com/aresky/bookingservice/service/account/AccountService.java
@@ -1,31 +1,29 @@
 package com.aresky.bookingservice.service.account;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import com.aresky.bookingservice.exception.BookingException;
-
+import jakarta.annotation.PostConstruct;
 import reactor.core.publisher.Mono;
 
 @Service
 public class AccountService {
 
-    private final String accountURL = "http://localhost:8082/api/v1/accounts";
+    private final String accountURL = "http://account-service:8082/api/v1/accounts";
 
-    @Autowired
-    private RestTemplate restTemplate;
+    private WebClient webClient;
+
+    @PostConstruct
+    private void initWebClient() {
+        this.webClient = WebClient.builder().baseUrl(accountURL).build();
+    }
 
     public Mono<Boolean> validateAccount(int accountId) {
-        String url = accountURL.concat("/validate").concat("?id=" + accountId);
-        return Mono.fromCallable(() -> restTemplate.getForEntity(url, Boolean.class))
-                .map(res -> {
-                    if (!res.getStatusCode().is2xxSuccessful()) {
-                        throw new BookingException("bad request to account-service");
-                    }
-
-                    return res.getBody();
-                })
-                .switchIfEmpty(Mono.error(new BookingException("Failed connect to account-service")));
+        String endpoint = "/validate";
+        return webClient
+                .get()
+                .uri(uri -> uri.path(endpoint).queryParam("id", accountId).build())
+                .retrieve()
+                .bodyToMono(Boolean.class);
     }
 }
