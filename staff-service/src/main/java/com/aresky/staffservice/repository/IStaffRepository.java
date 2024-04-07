f9b126266717165a29f3498c83b0306e8071ad8a
Aresky-T <tn6354103@gmail.com>
2024-04-07 23:51:36 +0700

    add methods to staff repo

--- staff-service/src/main/java/com/aresky/staffservice/repository/IStaffRepository.java
+++ staff-service/src/main/java/com/aresky/staffservice/repository/IStaffRepository.java
@@ -3,11 +3,36 @@ package com.aresky.staffservice.repository;
 import com.aresky.staffservice.model.Staff;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface IStaffRepository extends R2dbcRepository<Staff, Integer> {
+    Flux<Staff> findAllBy(Pageable pageable);
+
     Flux<Staff> findAllByDepartmentId(Integer departmentId);
 
     Flux<Staff> findAllByPositionId(Integer positionId);
+
+    Mono<Boolean> existsByEmail(String email);
+
+    Mono<Boolean> existsByPhone(String phone);
+
+    Mono<Boolean> existsByAccountId(Integer accountId);
+
+    Mono<Boolean> existsByContractUrl(String contractUrl);
+
+    Mono<Staff> findByAccountId(Integer accountId);
+
+    Mono<Staff> findByEmail(String email);
+
+    Mono<Staff> findByPhone(String phone);
+
+    Mono<Staff> findByContractUrl(String contractUrl);
+
+    @Query("SELECT CASE WHEN COUNT(*) > 0 FROM staff WHERE :fieldName = :value")
+    Mono<Boolean> existsByField(@Param("fieldName") String fieldName, @Param("value") Object value);
 }
