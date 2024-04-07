03964fa947908a756b13322b3cb47a5ddf951c8b
Aresky-T <tn6354103@gmail.com>
2024-04-08 03:25:28 +0700

    enable cors for controller

--- staff-service/src/main/java/com/aresky/staffservice/controller/DepartmentController.java
+++ staff-service/src/main/java/com/aresky/staffservice/controller/DepartmentController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -22,6 +23,7 @@ import com.aresky.staffservice.service.department.IDepartmentService;
 
 import reactor.core.publisher.Mono;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/departments")
 public class DepartmentController {
