f59a2a79483b62f52f6b1651c03ded2f5e12c3b3
Aresky-T <tn6354103@gmail.com>
2024-04-07 23:50:49 +0700

    remove mode status, table status, status dtos, repo and service related

--- staff-service/src/main/java/com/aresky/staffservice/repository/IStatusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aresky.staffservice.repository;
-
-import com.aresky.staffservice.model.Status;
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
-
-public interface IStatusRepository extends R2dbcRepository<Status, Integer> {
-}
