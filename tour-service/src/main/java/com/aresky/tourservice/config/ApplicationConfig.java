2e8b5d575b3e53f30320231bca15884ec7d656f1
Aresky-T <tn6354103@gmail.com>
2024-04-12 15:13:31 +0700

    add cors config for tour-service

--- tour-service/src/main/java/com/aresky/tourservice/config/ApplicationConfig.java
+++ tour-service/src/main/java/com/aresky/tourservice/config/ApplicationConfig.java
@@ -3,6 +3,9 @@ package com.aresky.tourservice.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ApplicationConfig {
@@ -10,5 +13,19 @@ public class ApplicationConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Key", "Authorization")
+                        .allowedMethods("POST", "PUT", "PATCH", "GET", "OPTIONS", "DELETE")
+                        .maxAge(3600);
+            }
+        };
     }
 }
