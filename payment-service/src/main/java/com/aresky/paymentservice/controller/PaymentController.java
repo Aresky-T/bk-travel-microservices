e10ff3b50b568ea49422c2bb98d6de32ac0b79a2
Aresky-T <tn6354103@gmail.com>
2024-04-15 03:31:37 +0700

    add new apis for payment-service

--- payment-service/src/main/java/com/aresky/paymentservice/controller/PaymentController.java
+++ payment-service/src/main/java/com/aresky/paymentservice/controller/PaymentController.java
@@ -2,35 +2,52 @@ package com.aresky.paymentservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aresky.paymentservice.dto.PaymentRequest;
+import com.aresky.paymentservice.dto.VnPayReturn;
+import com.aresky.paymentservice.model.Session;
 import com.aresky.paymentservice.service.vnpay.IVNPayService;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/v1/payment")
 public class PaymentController {
 
     @Autowired
-    private IVNPayService vPayService;
+    private IVNPayService vnPayService;;
+
+    @GetMapping("/connect")
+    public ResponseEntity<?> connect() {
+        return ResponseEntity.ok("Welcome to payment-service");
+    }
 
     @PostMapping("/vnpay")
     public ResponseEntity<String> paymentWithVNPay(
             @RequestBody PaymentRequest paymentInfo) {
-        String vnpayURL = vPayService.createOrder(paymentInfo);
+        String vnpayURL = vnPayService.createOrder(paymentInfo);
         return ResponseEntity.ok(vnpayURL);
     }
 
-    @GetMapping("/vnpay_return")
-    public ResponseEntity<?> vnPayPaymentReturn(HttpServletRequest request, Model model) {
-        return ResponseEntity.ok(vPayService.orderReturn(request));
+    @PostMapping("/vnpay/return")
+    public ResponseEntity<?> vnPayPaymentReturn(@RequestBody VnPayReturn result) {
+        return ResponseEntity.ok(vnPayService.orderReturn(result));
+    }
+
+    @PostMapping("/vnpay/session")
+    public ResponseEntity<Session> openSession(@RequestParam Integer bookingId) {
+        return ResponseEntity.ok(vnPayService.openSession(bookingId));
+    }
+
+    @DeleteMapping("/vnpay/session")
+    public ResponseEntity<?> closeSession(@RequestParam Integer bookingId) {
+        vnPayService.closeSession(bookingId);
+        return ResponseEntity.ok("success");
     }
 
 }
