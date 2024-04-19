4b5a85c7ed7a64536efb50d51fe2b8e794657f3a
Aresky-T <tn6354103@gmail.com>
2024-04-19 22:33:20 +0700

    gateway-service: update cors config for api gateway

--- gateway-service/src/main/java/com/aresky/gatewayservice/security/ReactiveWebSecurityConfig.java
+++ gateway-service/src/main/java/com/aresky/gatewayservice/security/ReactiveWebSecurityConfig.java
@@ -1,10 +1,11 @@
 package com.aresky.gatewayservice.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity.CorsSpec;
 import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
 import org.springframework.security.config.web.server.ServerHttpSecurity.FormLoginSpec;
 import org.springframework.security.config.web.server.ServerHttpSecurity.HttpBasicSpec;
@@ -12,12 +13,7 @@ import org.springframework.security.config.web.server.ServerHttpSecurity.LogoutS
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
-
-import java.util.*;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -25,49 +21,28 @@ public class ReactiveWebSecurityConfig {
 
     @Bean
     public SecurityWebFilterChain webFilterChain(ServerHttpSecurity http) throws Exception {
-        return http.cors(CorsSpec::disable)
+        return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(CsrfSpec::disable)
                 .formLogin(FormLoginSpec::disable)
                 .httpBasic(HttpBasicSpec::disable)
                 .logout(LogoutSpec::disable)
                 .authorizeExchange(exchange -> exchange.anyExchange().permitAll())
-                // .addFilterAt(corsWebFilter(), SecurityWebFiltersOrder.CORS)
                 .build();
     }
 
-    public CorsWebFilter corsWebFilter() {
-        return new CorsWebFilter(corsConfigurationSource());
-    }
-
+    @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        configuration.setMaxAge(8000L);
-        configuration.setAllowedHeaders(Arrays.asList("Origin", "X-Requested-With",
-                "Content-Type", "Accept", "Key",
-                "Authorization", "Requestor-Type"));
-        configuration.setExposedHeaders(Arrays.asList("X-Get-Header"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH",
-                "DELETE", "OPTIONS"));
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setMaxAge(8000L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+        source.registerCorsConfiguration("/**", corsConfiguration);
 
         return source;
-    }
-
-    @Bean
-    public WebFluxConfigurer corsConfigurer() {
-        return new WebFluxConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowCredentials(true)
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedHeaders("*")
-                        .allowedMethods("*");
-            }
-        };
     }
 }
