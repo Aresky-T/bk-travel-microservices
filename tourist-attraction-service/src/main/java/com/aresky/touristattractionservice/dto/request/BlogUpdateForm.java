d775e4428ae7cf6e4962ba8c956eaaa7f7afe6af
Aresky-T <tn6354103@gmail.com>
2024-04-25 19:06:07 +0700

    tourist-attraction-service: update dto classes

--- tourist-attraction-service/src/main/java/com/aresky/touristattractionservice/dto/request/BlogUpdateForm.java
+++ tourist-attraction-service/src/main/java/com/aresky/touristattractionservice/dto/request/BlogUpdateForm.java
@@ -1,5 +1,9 @@
 package com.aresky.touristattractionservice.dto.request;
 
+import java.util.List;
+
+import com.aresky.touristattractionservice.entity.BlogItem;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,4 +14,18 @@ public class BlogUpdateForm {
     private String imageUrl;
     private String intro;
     private String author;
+    private List<ItemDTO> items;
+
+    @Data
+    @NoArgsConstructor
+    public static class ItemDTO {
+        private Integer id;
+        private String subTitle;
+        private String imageUrl;
+        private String content;
+
+        public static BlogItem toBlogItem(ItemDTO dto) {
+            return new BlogItem(dto.id, dto.subTitle, dto.imageUrl, dto.content);
+        }
+    }
 }
