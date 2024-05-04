a7b7c12ad78cd7ab75e740760393e5946d44f499
Aresky-T <tn6354103@gmail.com>
2024-05-04 13:13:01 +0700

    account-service: add unit test for gRPC

--- account-service/src/test/java/com/aresky/accountservice/AccountServiceApplicationTests.java
+++ account-service/src/test/java/com/aresky/accountservice/AccountServiceApplicationTests.java
@@ -1,13 +1,36 @@
 package com.aresky.accountservice;
 
+import com.aresky.accountservice.grpc.account.CreateAccountRequest;
+import com.aresky.accountservice.grpc.account.CreateAccountResponse;
+import com.aresky.accountservice.grpc.account.ReactorAccountServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
 
 @SpringBootTest
 class AccountServiceApplicationTests {
+	private static ReactorAccountServiceGrpc.ReactorAccountServiceStub accountServiceStub;
 
-	@Test
-	void contextLoads() {
+	@BeforeAll
+	public static void setup(){
+		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
+		accountServiceStub = ReactorAccountServiceGrpc.newReactorStub(channel);
 	}
 
+	@Test
+	public void createAccount() {
+		System.out.println("Send request creating account");
+		CreateAccountRequest request = CreateAccountRequest.newBuilder()
+				.setUsername("aresky")
+				.setEmail("aresky@gmail.com")
+				.setPassword("110520")
+				.build();
+
+		accountServiceStub.createAccount(Mono.just(request))
+				.map(CreateAccountResponse::getMessage)
+				.map(System.out::printf);
+	}
 }
