f3a89473085133a59da2fcff521e3d54c86386bb
Aresky-T <tn6354103@gmail.com>
2024-05-09 18:30:09 +0700

    payment-service: updated mapper: clean SessionResponseMapper class

--- payment-service/src/main/java/com/aresky/paymentservice/grpc/mappers/SessionResponseMapper.java
+++ payment-service/src/main/java/com/aresky/paymentservice/grpc/mappers/SessionResponseMapper.java
@@ -1,21 +1,20 @@
 package com.aresky.paymentservice.grpc.mappers;
 
-import com.aresky.paymentservice.dto.request.BookingInfoReq;
 import com.aresky.paymentservice.model.Session;
 import grpc.payment.vnpay.SessionResponse;
 
 public class SessionResponseMapper {
-    public static SessionResponse mapFromSession(Session input){
+    public static SessionResponse mapFromSession(Session input) {
         SessionResponse.Builder builder = SessionResponse.newBuilder();
-        if(input.getBookingId() != null){
+        if (input.getBookingId() != null) {
             builder = builder.setBookingId(input.getBookingId());
         }
 
-        if(input.getTitle()!= null){
+        if (input.getTitle() != null) {
             builder = builder.setTitle(input.getTitle());
         }
 
-        if(input.getExpiration()!= null){
+        if (input.getExpiration() != null) {
             builder = builder.setExpiration(input.getExpiration().toString());
         }
 
