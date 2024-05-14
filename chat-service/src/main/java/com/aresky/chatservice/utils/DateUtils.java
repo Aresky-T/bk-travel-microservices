7f8a7b531c5436d2a7fa351d28ed285b3e91f81a
Aresky-T <tn6354103@gmail.com>
2024-05-14 13:02:50 +0700

    chat-service: add utilities classes

---
+++ chat-service/src/main/java/com/aresky/chatservice/utils/DateUtils.java
@@ -0,0 +1,31 @@
+package com.aresky.chatservice.utils;
+
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class DateUtils {
+    public static final DateTimeFormatter INSTANT_FORMATTER = DateTimeFormatter.ISO_INSTANT;
+    public static final DateTimeFormatter LOCAL_DATE_FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE;
+
+    public static String formatDateTime(ZonedDateTime date){
+        return INSTANT_FORMATTER.format(date);
+    }
+
+    public static String formatDateTime(Date date){
+        return INSTANT_FORMATTER.format(date.toInstant());
+    }
+
+    public static String formatDate(Date date){
+        return LOCAL_DATE_FORMATTER.format(new java.sql.Date(date.getTime()).toLocalDate());
+    }
+
+    public static String formatDate(LocalDate date){
+        return date.format(LOCAL_DATE_FORMATTER);
+    }
+
+    public static String format(ZonedDateTime date, DateTimeFormatter formatter){
+        return formatter.format(date);
+    }
+}
