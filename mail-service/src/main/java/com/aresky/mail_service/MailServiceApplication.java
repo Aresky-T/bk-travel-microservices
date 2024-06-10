1b2b36c2b942f7727ace9da9410b69dddfd6048c
Aresky-T <tn6354103@gmail.com>
2024-06-10 20:41:23 +0700

    mail-service: init

---
+++ mail-service/src/main/java/com/aresky/mail_service/MailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aresky.mail_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailServiceApplication.class, args);
+	}
+
+}
---
+++ mail-service/src/test/java/com/aresky/mail_service/MailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aresky.mail_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
