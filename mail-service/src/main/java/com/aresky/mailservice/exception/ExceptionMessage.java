0beb8522a724a18580be58e4ee3621feeafa2530
Aresky-T <tn6354103@gmail.com>
2024-06-17 16:39:01 +0700

    mail-service: add exception handle class

---
+++ mail-service/src/main/java/com/aresky/mailservice/exception/ExceptionMessage.java
@@ -0,0 +1,14 @@
+package com.aresky.mailservice.exception;
+
+public class ExceptionMessage {
+    public static final String STAFF_NOT_FOUND = "Staff not found!";
+    public static final String INVALID_STAFF_ID = "Invalid staffId!";
+    public static final String INVALID_CUSTOMER_ID = "Invalid customerId!";
+    public static final String INVALID_CUSTOMER_EMAIL = "Invalid customer email!";
+    public static final String INVALID_STAFF_EMAIL = "Invalid staffMail!";
+    public static final String STAFF_ALREADY_EXISTS = "Staff already exists!";
+    public static final String CUSTOMER_ALREADY_EXISTS = "Customer already exists!";
+    public static final String INVALID_MAIL_ID = "Invalid mailId!";
+    public static final String INVALID_MAILBOX_ID = "Invalid mailBoxId!";
+    public static final String ACCESS_DENIED_MESSAGE = "Access to mail has been restricted!";
+}
