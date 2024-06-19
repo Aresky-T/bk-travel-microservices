c0f4814d398166ec70d567e2f2cd6eef5b75cc55
Aresky-T <tn6354103@gmail.com>
2024-06-20 00:51:46 +0700

    review-service: update exception: add new constant messages for MessageResponse class

--- review-service/src/main/java/com/aresky/reviewservice/exception/MessageResponse.java
+++ review-service/src/main/java/com/aresky/reviewservice/exception/MessageResponse.java
@@ -1,11 +1,23 @@
 package com.aresky.reviewservice.exception;
 
+import com.aresky.reviewservice.entity.Reviewer;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import reactor.core.publisher.Mono;
 
 @Data
 @NoArgsConstructor
 public class MessageResponse {
+    public static final String INVALID_ACCOUNT_ID = "Invalid accountId";
+    public static final String INVALID_TOUR_ID = "Invalid tourId";
+    public static final String INVALID_SUB_TOUR_ID = "Invalid subTourId";
+    public static final String INVALID_STARS = "Invalid stars";
+    public static final String REVIEWED_THIS_TOUR = "This tour is reviewed!";
+    public static final String REVIEW_FAILED = "Failed review!";
+    public static final String STATISTIC_DOES_NOT_EXIST = "There are no review statistics available for this tour!";
+    public static final String INVALID_SUB_TOUR_ID_OR_ACCOUNT_ID = "Invalid accountId or subTourId!";
+    public static final String REVIEW_DOES_NOT_EXIST = "Review does not exist!";
+
     private String message;
 
     public MessageResponse(String message) {
