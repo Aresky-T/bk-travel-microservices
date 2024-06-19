0397908b3879d301766a8ec2d5cf5028b51e4af0
Aresky-T <tn6354103@gmail.com>
2024-06-20 00:59:54 +0700

    review-service: update repository: add new repository interface for Reviewer entity and add new abstract methods for other repository interfaces

--- review-service/src/main/java/com/aresky/reviewservice/repository/IReviewStatisticRepository.java
+++ review-service/src/main/java/com/aresky/reviewservice/repository/IReviewStatisticRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
 
 import com.aresky.reviewservice.entity.ReviewStatistic;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface IReviewStatisticRepository extends R2dbcRepository<ReviewStatistic, Integer> {
-
+    Mono<ReviewStatistic> findByTourId(Integer tourId);
 }
