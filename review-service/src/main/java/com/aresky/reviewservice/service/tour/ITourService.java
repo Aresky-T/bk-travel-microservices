b6c8538e4e7a816d4018876f572cffff3e834836
Aresky-T <tn6354103@gmail.com>
2024-06-20 01:03:17 +0700

    review-service: update service: implemented grpc methods to connect to tour-service via grpc protobuf

--- review-service/src/main/java/com/aresky/reviewservice/service/tour/ITourService.java
+++ review-service/src/main/java/com/aresky/reviewservice/service/tour/ITourService.java
@@ -1,5 +1,10 @@
 package com.aresky.reviewservice.service.tour;
 
-public interface ITourService {
+import grpc.tour.dto.response.SubTourResponse;
+import reactor.core.publisher.Mono;
 
+public interface ITourService {
+    Mono<Boolean> checkExistsTourById(Integer tourId);
+    Mono<Boolean> checkExistsSubTourById(Integer subTourId);
+    Mono<SubTourResponse> getSubTourById(Integer subTourId);
 }
