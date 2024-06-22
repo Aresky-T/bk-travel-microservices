39d09af5b824d3c8eb4420eaee5653ccb4b5106c
Aresky-T <tn6354103@gmail.com>
2024-06-22 19:48:08 +0700

    marking-service: add exception handle classes

---
+++ marking-service/src/main/java/com/aresky/markingservice/exception/MessageResponse.java
@@ -0,0 +1,22 @@
+package com.aresky.markingservice.exception;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MessageResponse {
+    public static final String INVALID_ACCOUNT_ID = "Invalid accountId";
+    public static final String INVALID_TOUR_ID = "Invalid tourId";
+    public static final String INVALID_SUB_TOUR_ID = "Invalid subTourId";
+    public static final String MARKED_THIS_TOUR = "This tour is marked!";
+    public static final String MARKING_FAILED = "Failed mark!";
+    public static final String STATISTIC_DOES_NOT_EXIST = "There are no marking statistics available for this tour!";
+    public static final String INVALID_SUB_TOUR_ID_OR_ACCOUNT_ID = "Invalid accountId or subTourId!";
+
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+}
