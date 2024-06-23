0d6be693d3c884a9d7a38ff8eeb945e2abb9818f
Aresky-T <tn6354103@gmail.com>
2024-06-23 21:09:46 +0700

    auth-service: update service: update AuthServiceImp class

--- auth-service/src/main/java/com/aresky/authservice/service/auth/AuthServiceImp.java
+++ auth-service/src/main/java/com/aresky/authservice/service/auth/AuthServiceImp.java
@@ -1,7 +1,19 @@
 package com.aresky.authservice.service.auth;
 
 import io.r2dbc.spi.Row;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,7 +27,9 @@ import com.aresky.authservice.jwt.JwtUtils;
 import com.aresky.authservice.repository.AuthRepository;
 import com.aresky.authservice.constants.ExceptionNotification;
 import com.aresky.authservice.service.account.IAccountGrpcService;
+import com.aresky.authservice.service.thymeleaf.IThymeleafService;
 
+import grpc.account.v2.dto.response.AccountResponse;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -28,10 +42,19 @@ public class AuthServiceImp implements IAuthService {
     private IAccountGrpcService accountGrpcService;
 
     @Autowired
+    private IThymeleafService thymeleafService;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Autowired
     private JwtUtils jwtUtils;
 
     @Autowired
     public DatabaseClient databaseClient;
+
+    @Value("${spring.mail.username}")
+    private String email;
 
     @Transactional
     @Override
@@ -45,7 +68,12 @@ public class AuthServiceImp implements IAuthService {
                 .flatMap(existsAccount -> accountGrpcService.getAccountByUsernameAndPassword(username, password))
                 .switchIfEmpty(Mono.error(new AuthException(ExceptionNotification.INVALID_PASSWORD)))
                 .flatMap(account -> {
-                    String token = jwtUtils.generateToken(account.getUsername());
+                    Map<String, Object> props = new HashMap<>();
+                    props.put("sub", username);
+                    props.put("email", account.getEmail());
+                    props.put("role", account.getRole());
+
+                    String token = jwtUtils.generateToken(props);
                     LoginResponse loginResponse = LoginResponse
                             .builder()
                             .id(account.getId())
@@ -57,7 +85,7 @@ public class AuthServiceImp implements IAuthService {
                             .status(account.getStatus())
                             .build();
 
-                    return this.findByAccountId(account.getId())
+                    return this.findAuthByAccountId(account.getId())
                             .switchIfEmpty(authRepository.save(new Auth(account.getId(), token)))
                             .flatMap(auth -> {
                                 auth.setAccessToken(token);
@@ -68,8 +96,29 @@ public class AuthServiceImp implements IAuthService {
     }
 
     @Override
-    public Mono<Boolean> isValidAccessToken(String token) {
-        return Mono.just(jwtUtils.isValidToken(token));
+    public Mono<Boolean> validateAccessToken(String token) {
+        return Mono.just(jwtUtils.isValidToken(token))
+                .flatMap(isValid -> {
+                    if (!isValid)
+                        return Mono.just(false);
+
+                    String email = jwtUtils.getClaimsFromToken(token).get("email", String.class);
+                    System.out.println("Subject: " + jwtUtils.getClaimsFromToken(token).getSubject());
+                    System.out.println("Email: " + email);
+
+                    return Mono.justOrEmpty(email)
+                            .flatMap(accountGrpcService::getAccountByEmail)
+                            .switchIfEmpty(Mono.error(new AuthException(ExceptionNotification.ACCOUNT_NOT_EXISTS)))
+                            .map(AccountResponse::getId)
+                            .flatMap(authRepository::findByAccountId)
+                            .map(auth -> {
+                                if (auth.getAccessToken().equals(token)) {
+                                    return true;
+                                }
+
+                                return false;
+                            });
+                });
     }
 
     @Transactional
@@ -81,34 +130,37 @@ public class AuthServiceImp implements IAuthService {
                 .thenReturn("success");
     }
 
-    public Mono<Auth> findByAccountId(int accountId) {
+    @Override
+    public Mono<String> handleForgotPassword(String email) {
+        return accountGrpcService.checkAccountByEmail(email)
+                .filter(Boolean.TRUE::equals)
+                .switchIfEmpty(Mono.error(new AuthException(ExceptionNotification.ACCOUNT_NOT_EXISTS)))
+                .then(accountGrpcService.resetPassword(email))
+                .flatMap(newPassword -> {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
+                    String now = dateFormat.format(new Date(System.currentTimeMillis()));
+
+                    // Add variable to context thymeleaf
+                    Map<String, Object> variables = new HashMap<>();
+                    variables.put("requiredResetPasswordTime", now);
+                    variables.put("newPasswordRandom", newPassword);
+                    // Send email
+                    final String FORGOT_PASSWORD_TEMPLATE_FILE_NAME = "forgot-password-template";
+                    String template = thymeleafService.createContent(FORGOT_PASSWORD_TEMPLATE_FILE_NAME, variables);
+
+                    return sendMailWithHtmlTemplate(
+                            email,
+                            "BK TRAVEL - Yêu cầu đặt lại mật khẩu",
+                            template)
+                            .thenReturn("success");
+                });
+    }
+
+    public Mono<Auth> findAuthByAccountId(int accountId) {
         String query = "SELECT * FROM auth WHERE account_id = :accountId";
         return databaseClient.sql(query).bind("accountId", accountId)
                 .map(((row, rowMetadata) -> mapRowToAuth(row)))
                 .one();
-    }
-
-    @Override
-    public Mono<Auth> findByUsername(String username) {
-        return Mono.empty();
-    }
-
-    @Override
-    public Mono<Void> save(Auth auth) {
-        if (auth == null) {
-            throw new AuthException(ExceptionNotification.BAD_CREDENTIALS);
-        }
-        return authRepository.save(auth).then();
-    }
-
-    @Override
-    public Mono<Boolean> isExistsByUsername(String username) {
-        return Mono.just(false);
-    }
-
-    @Override
-    public Mono<Boolean> isExistsByEmail(String email) {
-        return Mono.just(false);
     }
 
     private Auth mapRowToAuth(Row row) {
@@ -119,8 +171,20 @@ public class AuthServiceImp implements IAuthService {
         return auth;
     }
 
-    @Override
-    public Mono<String> handleForgotPassword(String email) {
-        return accountGrpcService.resetPassword(email);
+    private Mono<Void> sendMailWithHtmlTemplate(String recipient, String subject, String body) {
+        return Mono.fromRunnable(() -> {
+            try {
+                MimeMessage message = javaMailSender.createMimeMessage();
+                MimeMessageHelper helper = new MimeMessageHelper(message,
+                        MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, "UTF-8");
+                helper.setTo(recipient);
+                helper.setFrom(email, "Bk Travel");
+                helper.setSubject(subject);
+                helper.setText(body, true);
+                javaMailSender.send(message);
+            } catch (MessagingException | UnsupportedEncodingException ex) {
+                ex.printStackTrace(System.err);
+            }
+        });
     }
 }
