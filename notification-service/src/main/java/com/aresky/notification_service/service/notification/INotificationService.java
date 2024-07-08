41c92b45b07bc1a7c6f13db5416366e6d4a857c8
Aresky-T <tn6354103@gmail.com>
2024-07-08 20:25:29 +0700

    notification-service: update service/notification: add new abstract methods for service interface INotificationService and their implementation code in the NotificationServiceImp class

--- notification-service/src/main/java/com/aresky/notification_service/service/notification/INotificationService.java
+++ notification-service/src/main/java/com/aresky/notification_service/service/notification/INotificationService.java
@@ -4,12 +4,15 @@ import com.aresky.notification_service.dto.request.NotificationRequest;
 import com.aresky.notification_service.dto.request.TypeRequest;
 import com.aresky.notification_service.dto.response.NotificationResponse;
 import com.aresky.notification_service.dto.response.NotificationTypeResponse;
+import com.aresky.notification_service.dto.response.UserNotification;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface INotificationService {
-    Mono<List<NotificationResponse>> getAllNotifications(Integer accountId);
+    Mono<Long> getNewNotificationCount(Integer userId);
+    Mono<List<NotificationResponse>> getAllNotifications(Integer accountId,  Integer limit, Integer offset);
+    Mono<UserNotification> getUserNotificationDto(Integer accountId, Integer page, Integer size);
     Mono<NotificationTypeResponse> getNotificationTypeResponse(Integer typeId);
     Mono<NotificationTypeResponse> getNotificationTypeResponse(String typeName);
     Mono<List<NotificationTypeResponse>> getAllNotificationTypesByEntityType(String entityType);
@@ -17,8 +20,11 @@ public interface INotificationService {
     Mono<Void> createNotification(Mono<NotificationRequest> request);
     Mono<Void> createNotificationType(TypeRequest request);
     Mono<Void> readNotification(Integer notificationId);
+    Mono<Void> readAllNotifications(Integer userId);
+    Mono<Void> viewNotification(Integer userId);
     Mono<Void> addKeyWord(Integer notificationTypeId, String keyword);
     Mono<Void> removeKeyWord(Integer notificationTypeId, String keyword);
     Mono<Void> updateTemplate(Integer notificationTypeId, String template);
     Mono<Void> removeNotificationType(Integer typeId);
+    Mono<Void> removeNotification(Integer notificationId);
 }
