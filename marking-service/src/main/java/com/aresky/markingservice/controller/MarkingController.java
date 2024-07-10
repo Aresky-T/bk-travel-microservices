3b306d9eaea1e680ebf7915854b7ffe0e75dfc9e
Aresky-T <tn6354103@gmail.com>
2024-07-10 17:00:59 +0700

    marking-service: update controller: add the new getAllMarkedTourResponses api method with endpoint "/all" for MarkingController class

--- marking-service/src/main/java/com/aresky/markingservice/controller/MarkingController.java
+++ marking-service/src/main/java/com/aresky/markingservice/controller/MarkingController.java
@@ -1,17 +1,25 @@
 package com.aresky.markingservice.controller;
 
 import com.aresky.markingservice.dto.request.MarkedTourRequest;
+import com.aresky.markingservice.dto.response.MarkedTourResponse;
 import com.aresky.markingservice.service.marking.IMarkingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/marking")
 public class MarkingController {
     @Autowired
     private IMarkingService markingService;
+
+    @GetMapping("/all")
+    public Mono<ResponseEntity<List<MarkedTourResponse>>> getAllMarkedTourResponses(@RequestParam Integer accountId){
+        return markingService.getAllMarkedTourResponses(accountId).map(ResponseEntity::ok);
+    }
 
     @GetMapping("/check")
     public Mono<ResponseEntity<Boolean>> checkMarkedTour(
@@ -27,7 +35,10 @@ public class MarkingController {
     }
 
     @DeleteMapping
-    public Mono<ResponseEntity<?>> unmarkTour(@RequestParam Integer markedTourId){
-        return markingService.unmarkTour(markedTourId).thenReturn(ResponseEntity.ok("success"));
+    public Mono<ResponseEntity<?>> unmarkTour(
+            @RequestParam Integer accountId,
+            @RequestParam Integer subTourId
+    ){
+        return markingService.unmarkTour(accountId, subTourId).thenReturn(ResponseEntity.ok("success"));
     }
 }
