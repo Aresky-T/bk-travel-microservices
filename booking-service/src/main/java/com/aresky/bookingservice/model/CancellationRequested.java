a475f88c7019ec3457e2b1a21c66deda6c2211f9
Aresky-T <tn6354103@gmail.com>
2024-07-11 04:30:07 +0700

    booking-service: updated entity: add new CancellationRequested class

--- booking-service/src/main/java/com/aresky/bookingservice/model/CancellationRequested.java
+++ booking-service/src/main/java/com/aresky/bookingservice/model/CancellationRequested.java
@@ -1,13 +1,12 @@
 package com.aresky.bookingservice.model;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.Table;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.ZonedDateTime;
 
@@ -15,8 +14,8 @@ import java.time.ZonedDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table("request_cancel_booking")
-public class RequestCancelBooking {
+@Table("cancellation_requested")
+public class CancellationRequested {
     @Id
     private Integer id;
 
