50a517b0321d2b0c5cb7548e22089ef9331af773
Aresky-T <tn6354103@gmail.com>
2024-07-10 16:57:01 +0700

    marking-service: update dto: add the subTour property for the MarkedTourResponse dto class

--- marking-service/src/main/java/com/aresky/markingservice/dto/response/MarkedTourResponse.java
+++ marking-service/src/main/java/com/aresky/markingservice/dto/response/MarkedTourResponse.java
@@ -2,6 +2,7 @@ package com.aresky.markingservice.dto.response;
 
 import com.aresky.markingservice.entity.MarkedTour;
 import com.aresky.markingservice.utils.DateUtils;
+import grpc.tour.dto.response.SubTourResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,6 +17,7 @@ public class MarkedTourResponse {
     private Integer accountId;
     private Integer subTourId;
     private String markedAt;
+    private SubTour subTour;
 
     public static MarkedTourResponse toDto(MarkedTour markedTour){
         return MarkedTourResponse.builder()
@@ -24,5 +26,44 @@ public class MarkedTourResponse {
                .subTourId(markedTour.getSubTourId())
                .markedAt(DateUtils.formatDateTime(markedTour.getMarkedAt()))
                .build();
+    }
+
+    public MarkedTourResponse subTour(SubTour subTour){
+        this.subTour = subTour;
+        return this;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SubTour {
+        private Integer id;
+        private Integer tourId;
+        private String title;
+        private String tourCode;
+        private String image1;
+        private String duration;
+        private String departureTime;
+        private String departureLocation;
+        private String vehicle;
+        private Integer availableSeats;
+        private Integer adultPrice;
+
+        public static SubTour fromGrpcSubTourResponse(SubTourResponse input){
+            return SubTour.builder()
+                   .id(input.getId())
+                   .tourId(input.getTourId())
+                   .title(input.getTitle())
+                   .tourCode(input.getTourCode())
+                   .image1(input.getImage1())
+                   .duration(input.getDuration())
+                   .departureTime(input.getDepartureTime())
+                   .departureLocation(input.getDepartureLocation())
+                   .vehicle(input.getVehicle())
+                   .availableSeats(input.getAvailableSeats())
+                   .adultPrice(input.getAdultPrice())
+                   .build();
+        }
     }
 }
