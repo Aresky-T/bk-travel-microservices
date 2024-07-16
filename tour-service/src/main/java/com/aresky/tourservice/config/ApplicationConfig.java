a1573e8364a15189af073677d612e09e5f2ea2bd
Aresky-T <tn6354103@gmail.com>
2024-07-16 17:37:03 +0700

    tour-service: update config: add the new CloudinaryConfig configuration class

--- tour-service/src/main/java/com/aresky/tourservice/config/ApplicationConfig.java
+++ tour-service/src/main/java/com/aresky/tourservice/config/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package com.aresky.tourservice.config;
 
+import com.cloudinary.Cloudinary;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +11,10 @@ public class ApplicationConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public Cloudinary cloudinary(CloudinaryConfig config){
+        return config.getCloudinary();
     }
 }
