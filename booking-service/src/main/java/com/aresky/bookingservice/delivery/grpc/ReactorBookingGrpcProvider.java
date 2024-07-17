04e1f8eed436322c02c3eb79f9ae8efa239485fb
Aresky-T <tn6354103@gmail.com>
2024-07-17 17:59:46 +0700

    booking-service: update delivery/grpc: apply global interceptor annotation for BookingGrpcInterceptor class

--- booking-service/src/main/java/com/aresky/bookingservice/delivery/grpc/ReactorBookingGrpcProvider.java
+++ booking-service/src/main/java/com/aresky/bookingservice/delivery/grpc/ReactorBookingGrpcProvider.java
@@ -17,7 +17,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.Objects;
 
-@GrpcService(interceptors = {BookingGrpcInterceptor.class})
+@GrpcService
 public class ReactorBookingGrpcProvider extends ReactorBookingServiceGrpc.BookingServiceImplBase {
     @Autowired
     private IBookingService bookingService;
