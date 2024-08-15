ee3f2a5a6ed4a648fa1e775287235e5ea482d471
Aresky-T <tn6354103@gmail.com>
2024-08-15 21:08:12 +0700

    payment-service: update kafka: add new topic property for the KafkaTopic class

--- payment-service/src/main/java/com/aresky/paymentservice/kafka/KafkaTopic.java
+++ payment-service/src/main/java/com/aresky/paymentservice/kafka/KafkaTopic.java
@@ -4,4 +4,5 @@ public class KafkaTopic {
     public static final String BOOKING_WITH_VNPAY_SUCCESS = "BOOKING_WITH_VNPAY_SUCCESS";
     public static final String BOOKING_WITH_VNPAY_FAILED = "BOOKING_WITH_VNPAY_FAILED";
     public static final String BOOKING_WITH_VNPAY_CANCELLED = "BOOKING_WITH_VNPAY_CANCELLED";
+    public static final String VNPAY_PAYMENT_SUCCESS = "VNPAY_PAYMENT_SUCCESS";
 }
