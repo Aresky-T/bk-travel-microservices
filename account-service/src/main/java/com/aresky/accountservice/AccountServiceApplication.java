f73fe2d6e942ec54e8fb661dfa0849452f490f61
Aresky-T <tn6354103@gmail.com>
2024-08-15 21:32:33 +0700

    account-service: update class AccountServiceApplication

--- account-service/src/main/java/com/aresky/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/aresky/accountservice/AccountServiceApplication.java
@@ -1,11 +1,16 @@
 package com.aresky.accountservice;
 
+import com.aresky.accountservice.repository.AccountRepository;
+import com.aresky.accountservice.repository.ProfileRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
-@EnableR2dbcRepositories
+@EnableR2dbcRepositories(basePackageClasses = {
+		AccountRepository.class,
+		ProfileRepository.class
+})
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AccountServiceApplication {
