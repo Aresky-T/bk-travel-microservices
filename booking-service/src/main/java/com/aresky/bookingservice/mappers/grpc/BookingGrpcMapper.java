8283261fefcfecdc0e0f91d981a04421a3da8b88
Aresky-T <tn6354103@gmail.com>
2024-08-15 21:47:13 +0700

    booking-service: modified mapper/grpc: class BookingGrpcMapper

--- booking-service/src/main/java/com/aresky/bookingservice/mappers/grpc/BookingGrpcMapper.java
+++ booking-service/src/main/java/com/aresky/bookingservice/mappers/grpc/BookingGrpcMapper.java
@@ -1,8 +1,10 @@
 package com.aresky.bookingservice.mappers.grpc;
 
 import com.aresky.bookingservice.model.Booking;
+import com.aresky.bookingservice.model.VnPayTransactionInfo;
 import grpc.booking.BookingResponse;
 import grpc.booking.constants.BookingStatus;
+import grpc.payment.vnpay.TransactionInfoResponse;
 
 public class BookingGrpcMapper {
 
@@ -13,6 +15,18 @@ public class BookingGrpcMapper {
                 .setTourCode(booking.getTourCode())
                 .setAmount(booking.getAmount())
                 .setStatus(BookingStatus.valueOf(booking.getStatus().name()))
+                .build();
+    }
+
+    public static VnPayTransactionInfo mapToVnPayTransactionInfo(TransactionInfoResponse input){
+        return VnPayTransactionInfo.builder()
+                .bank(input.getBank())
+                .cardType(input.getCardType())
+                .orderInfo(input.getOrderInfo())
+                .payDate(input.getPayDate())
+                .transactionNo(input.getTransactionNo())
+                .txnRef(input.getTxnRef())
+                .amount(input.getAmount())
                 .build();
     }
 }
