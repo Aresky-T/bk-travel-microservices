ab5a14603144c17404f5bdc0450e0b24c0228caf
Aresky-T <tn6354103@gmail.com>
2024-08-15 21:29:55 +0700

    account-service: update repository: interface AccountRepository and interface ProfileRepository

--- account-service/src/main/java/com/aresky/accountservice/repository/AccountRepository.java
+++ account-service/src/main/java/com/aresky/accountservice/repository/AccountRepository.java
@@ -2,14 +2,12 @@ package com.aresky.accountservice.repository;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
-import org.springframework.stereotype.Repository;
 
 import com.aresky.accountservice.model.Account;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@Repository
 public interface AccountRepository extends R2dbcRepository<Account, Integer> {
     Flux<Account> findAllBy(Pageable pageable);
 
--- account-service/src/main/java/com/aresky/accountservice/repository/ProfileRepository.java
+++ account-service/src/main/java/com/aresky/accountservice/repository/ProfileRepository.java
@@ -1,13 +1,11 @@
 package com.aresky.accountservice.repository;
 
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
-import org.springframework.stereotype.Repository;
 
 import com.aresky.accountservice.model.Profile;
 
 import reactor.core.publisher.Mono;
 
-@Repository
 public interface ProfileRepository extends R2dbcRepository<Profile, Integer> {
     Mono<Profile> findByAccountId(int accountId);
 
