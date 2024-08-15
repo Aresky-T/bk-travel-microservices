9785ab1f83758260d990643f01b0fcf7fcc244bd
Aresky-T <tn6354103@gmail.com>
2024-08-15 21:46:21 +0700

    booking-service: modified repository: interface CancellationRequestedRepository

--- booking-service/src/main/java/com/aresky/bookingservice/repository/CancellationRequestedRepository.java
+++ booking-service/src/main/java/com/aresky/bookingservice/repository/CancellationRequestedRepository.java
@@ -2,7 +2,11 @@ package com.aresky.bookingservice.repository;
 
 import com.aresky.bookingservice.model.CancellationRequested;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Mono;
 
 public interface CancellationRequestedRepository extends R2dbcRepository<CancellationRequested, Integer> {
 
+    Mono<CancellationRequested> findByBookingId(Integer bookingId);
+
+    Mono<Boolean> existsByBookingId(Integer bookingId);
 }
