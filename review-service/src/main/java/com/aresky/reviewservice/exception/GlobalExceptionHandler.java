0b3a51c9edf1d74d995857c7a2338df2ef9f6f41
Aresky-T <tn6354103@gmail.com>
2024-08-15 22:10:44 +0700

    review-service: modified exception: class GlobalExceptionHandler

--- review-service/src/main/java/com/aresky/reviewservice/exception/GlobalExceptionHandler.java
+++ review-service/src/main/java/com/aresky/reviewservice/exception/GlobalExceptionHandler.java
@@ -2,9 +2,19 @@ package com.aresky.reviewservice.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,5 +24,32 @@ public class GlobalExceptionHandler {
     public ResponseEntity<MessageResponse> handlingReviewException(ReviewException ex) {
         logger.error("ReviewException: {}", ex.getMessage());
         return ResponseEntity.badRequest().body(new MessageResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<?> handlingWebExchangeBindException(WebExchangeBindException ex){
+        List<String> errors = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handlingMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        List<String> errors = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .toList();
+
+        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    private Map<String, List<String>> getErrorsMap(List<String> errors){
+        Map<String, List<String>> errorResponse = new HashMap<>();
+        errorResponse.put("errors", errors);
+        return errorResponse;
     }
 }
