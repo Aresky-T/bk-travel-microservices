e19e26d32c27de69c10921d2e5d265fe41f8f78b
Aresky-T <tn6354103@gmail.com>
2024-10-01 02:19:08 +0700

    release/v1.0.1: payment-service: updated service

--- payment-service/src/main/java/com/aresky/paymentservice/service/booking/IBookingService.java
+++ payment-service/src/main/java/com/aresky/paymentservice/service/booking/IBookingService.java
@@ -1,12 +1,12 @@
 package com.aresky.paymentservice.service.booking;
 
-import grpc.booking.constants.PaymentMethod;
+import grpc.booking.v2.constants.PaymentMethod;
 import grpc.booking.v2.model.Booking;
 
 public interface IBookingService {
-    boolean checkExistBookingBy(Integer bookingId);
-    void updateBookingAfterPaymentSucceeded(Integer bookingId, PaymentMethod method);
-    void updateBookingAfterPaymentFailed(Integer bookingId, PaymentMethod method);
-    void updateBookingAfterPaymentCanceled(Integer bookingId);
+//    boolean checkExistBookingBy(Integer bookingId);
+//    void updateBookingAfterPaymentSucceeded(Integer bookingId, PaymentMethod method);
+//    void updateBookingAfterPaymentFailed(Integer bookingId, PaymentMethod method);
+//    void updateBookingAfterPaymentCanceled(Integer bookingId);
     Booking getBookingById(Integer bookingId);
 }
